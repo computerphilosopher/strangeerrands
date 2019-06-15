5e7b6e6a8deacd6c4712be7228725ad2aaa1fd71
computerphilosopher <abc3242@gmail.com>
2019-06-15 18:12:36 +0900

    심부름 목록 DB 연동 성공

--- Assets/Scripts/QuestSceneHandler.cs
+++ Assets/Scripts/QuestSceneHandler.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 
 public class QuestSceneHandler : MonoBehaviour
 {
     // Start is called before the first frame update
+    StageHandler stageHandler;
     void Start()
     {
+        this.stageHandler = StageHandler.Handler;
+        Stage currentStage = stageHandler.CurrentStage;
+
+        Debug.Log(currentStage.Hint);
+
+        Text description = GameObject.Find("Description").GetComponent<Text>();
+        Text Hint = GameObject.Find("Hint").GetComponent<Text>();
+
+        description.text = currentStage.Description;
+        Hint.text = currentStage.Hint;
+        
     }
 
     // Update is called once per frame
@@ -15,7 +29,7 @@ public class QuestSceneHandler : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            SceneManager.LoadScene("SampleScene");
+            SceneManager.LoadScene("shop");
         }
         
     }
--- Assets/Scripts/StageHandler.cs
+++ Assets/Scripts/StageHandler.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using Mono.Data.Sqlite;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StageHandler
 {
-    private StageHandler instance = null;
-    
+    private static StageHandler instance = null;
 
-    public StageHandler stageHandelr
+    public Stage CurrentStage
+    {
+        get;
+        set;
+    }
+
+    public static StageHandler Handler 
 
     {
         get
@@ -20,25 +28,95 @@ public class StageHandler
         }
     }
 
-    private int currentStageID;
 
     private StageHandler()
     {
+        CurrentStage = new Stage(1);
     }
 
-    class Stage
+    public Stage GetStage(int id)
     {
-        public int ID{
-            get;
-        }
-
-        public string Description{
-            get;
-        }
-
-        public string Hint{
-            get;
-        }
+        return new Stage(id);
     }
+
 
 }
+public class Stage
+{
+    public int ID
+    {
+        get;
+    }
+
+    public string Description
+    {
+        get;
+    }
+
+    public string Hint
+    {
+        get;
+    }
+
+    public Sprite FailImage
+    {
+        get;
+    }
+
+    public string FailText
+    {
+        get;
+    }
+
+    public Sprite SuccessImage
+    {
+        get;
+    }
+
+    public string SuccessText
+    {
+        get;
+    }
+
+
+    public Stage(int id)
+    {
+        string conn = "URI=file:" + Application.dataPath + "/DB/" + "Question.db";
+
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open();
+
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        string sqlQuery = "SELECT * FROM Question WHERE ID =" + id.ToString();
+        dbcmd.CommandText = sqlQuery;
+
+        IDataReader reader = dbcmd.ExecuteReader();
+
+
+        while (reader.Read())
+        {
+            ID = reader.GetInt32(0);
+            Description = reader.GetString(1);
+            Hint = reader.GetString(2);
+
+            byte[] failJpg = (byte[])reader["FailImage"];
+            Texture2D failTex = new Texture2D(500, 500);
+            failTex.LoadImage(failJpg);
+            FailImage = Sprite.Create(failTex, new Rect(0, 0, 400, 400), new Vector2(0.5f, 0.5f));
+
+            FailText = reader.GetString(4);
+
+            byte[] successJpg = (byte[])reader["SuccessImage"];
+            Texture2D successTex = new Texture2D(500, 500);
+            successTex.LoadImage(successJpg);
+            SuccessImage = Sprite.Create(successTex, new Rect(0, 0, 150, 150), new Vector2(0.5f, 0.5f));
+
+            SuccessText = reader.GetString(6);
+        }
+        dbconn.Close();
+        reader.Dispose();
+    }
+}
+
+
